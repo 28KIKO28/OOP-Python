99c6ac26f78738fed5ea9c4151e45bf2537d8ff3
28KIKO28 <franicolau2@gmail.com>
2026-01-29 21:38:38 +0000

    Add files via upload

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -1,28 +1,17 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-turma1 = ["Filipe", "Ana", "Carla", "Joana", "Mariana", "Rafael", "Beatriz", "Gustavo", "Fernanda", "Lucas", "Samantha", "Tiago", "Camila", "Pedro", "Juliana", "Bruno", "Larissa", "Diego", "Amanda", "Felipe"]
-turma2 = ["Marcos", "Paula", "Juliana", "Camila", "Bruno", "Larissa", "Diego", "Amanda", "Felipe", "Renata", "César", "Vivian", "Rodrigo", "Patrícia", "Eduardo", "Simone", "Fábio", "Aline", "Guilherme", "Isabela"]
-turma3 = ["Sofia", "Matheus", "Isabela", "Gabriel", "Yasmin", "Thiago", "Lara", "Vitor", "Nicole", "Enzo", "Luna", "Davi", "Helena", "Samuel", "Clara", "Rafael", "Marina", "Cecília", "Guilherme", "Alice"]
-turma4 = ["Eduardo" , "Bianca" , "Felipe" , "Larissa" , "Rafael" , "Amanda" , "Gustavo" , "Isadora" , "Bruna" , "Caio" , "Letícia" , "Vinícius", "Mariana", "Leonardo", "Sabrina", "Daniel", "Camila", "Bruno", "Natália", "Lucas"]
-turma5 = ["Renan", "Sabrina", "Thiago", "Vanessa", "Wesley", "Ximena", "Yago", "Zara", "Aline", "Brenda", "Caetano", "Daniel", "Emanuel", "Fabiana", "Geraldo", "Heloísa", "Igor", "Jéssica", "Kléber", "Larissa"]
-turma6 = ["Mário", "Natasha", "Olívia", "Júlia", "Kauã", "Lívia", "Murilo", "Nina", "Otávio", "Pietro", "Quirino", "Rosa", "Sandro", "Tatiana", "Ubirajara", "Valentina", "William", "Xandra", "Yasmin", "Zeca"]
-turma7 = ["Vanessa", "Wesley", "Ximena", "Yago", "Zara", "Aline", "Brenda", "Caetano", "Daniel", "Emanuel", "Fabiana", "Geraldo", "Heloísa", "Igor", "Jéssica", "Kléber", "Larissa", "Mário", "Natasha", "Olívia"]
-turma8 = ["Júlia", "Kauã", "Lívia", "Murilo", "Nina", "Otávio", "Pietro", "Quirino", "Rosa", "Sandro", "Tatiana", "Ubirajara", "Valentina", "William", "Xandra", "Yasmin", "Zeca", "Alice", "Bruno", "Catarina"]
-turma9 = ["Ursula", "Vitor", "Wagner", "Xuxa", "Yuri", "Zélia", "Amanda", "Bruno", "Camila", "Diego", "Eduarda", "Fábio", "Gabriela", "Hugo", "Isadora", "João", "Kátia", "Leonardo", "Marina", "Nicolas"]
-
 lista_turmas = [
-    (10, 1, 20, "Joaquim", turma1), 
-    (10, 2, 20, "Pedro", turma2), 
-    (10, 3, 20, "Benedito", turma3),
-    (10, 4, 20, "Mariana", turma4),
-    (10, 5, 20, "Lucas", turma5),
-    (10, 6, 20, "Fernanda", turma6),
-    (10, 7, 20, "Ricardo", turma7),
-    (10, 8, 20, "Sílvia", turma8),
-    (10, 9, 20, "Tatiana", turma9)
+    (10, 1, 20, "Joaquim", ["Filipe", "Ana", "Carla", "Joana", "Mariana", "Rafael", "Beatriz", "Gustavo", "Fernanda", "Lucas", "Samantha", "Tiago", "Camila", "Pedro", "Juliana", "Bruno", "Larissa", "Diego", "Amanda", "Felipe"]), 
+    (10, 2, 20, "Pedro", ["Marcos", "Paula", "Juliana", "Camila", "Bruno", "Larissa", "Diego", "Amanda", "Felipe", "Renata", "César", "Vivian", "Rodrigo", "Patrícia", "Eduardo", "Simone", "Fábio", "Aline", "Guilherme", "Isabela"]), 
+    (10, 3, 20, "Benedito", ["Sofia", "Matheus", "Isabela", "Gabriel", "Yasmin", "Thiago", "Lara", "Vitor", "Nicole", "Enzo", "Luna", "Davi", "Helena", "Samuel", "Clara", "Rafael", "Marina", "Cecília", "Guilherme", "Alice"]),
+    (10, 4, 20, "Mariana", ["Eduardo" , "Bianca" , "Felipe" , "Larissa" , "Rafael" , "Amanda" , "Gustavo" , "Isadora" , "Bruna" , "Caio" , "Letícia" , "Vinícius", "Mariana", "Leonardo", "Sabrina", "Daniel", "Camila", "Bruno", "Natália", "Lucas"]),
+    (10, 5, 20, "Lucas", ["Renan", "Sabrina", "Thiago", "Vanessa", "Wesley", "Ximena", "Yago", "Zara", "Aline", "Brenda", "Caetano", "Daniel", "Emanuel", "Fabiana", "Geraldo", "Heloísa", "Igor", "Jéssica", "Kléber", "Larissa"]),
+    (10, 6, 20, "Fernanda", ["Mário", "Natasha", "Olívia", "Júlia", "Kauã", "Lívia", "Murilo", "Nina", "Otávio", "Pietro", "Quirino", "Rosa", "Sandro", "Tatiana", "Ubirajara", "Valentina", "William", "Xandra", "Yasmin", "Zeca"]),
+    (10, 7, 20, "Ricardo", ["Vanessa", "Wesley", "Ximena", "Yago", "Zara", "Aline", "Brenda", "Caetano", "Daniel", "Emanuel", "Fabiana", "Geraldo", "Heloísa", "Igor", "Jéssica", "Kléber", "Larissa", "Mário", "Natasha", "Olívia"]),
+    (10, 8, 20, "Sílvia", ["Júlia", "Kauã", "Lívia", "Murilo", "Nina", "Otávio", "Pietro", "Quirino", "Rosa", "Sandro", "Tatiana", "Ubirajara", "Valentina", "William", "Xandra", "Yasmin", "Zeca", "Alice", "Bruno", "Catarina"]),
+    (10, 9, 20, "Filipe", ["Tatiana", "Ursula", "Vitor", "Wagner", "Xuxa", "Yuri", "Zélia", "Amanda", "Bruno", "Camila", "Diego", "Eduarda", "Fábio", "Gabriela", "Hugo", "Isadora", "João", "Kátia", "Leonardo", "Marina", "Nicolas"])
 ]
 @app.route("/")
 def turmas ():
@@ -33,5 +22,12 @@ def alunos (indice):
     turma = lista_turmas[indice]
     alunos = sorted(turma[4])
     return render_template ("alunos.html", turma=turma, alunos=alunos)
+
+
+@app.route("/tarefas")
+def tarefas():
+    return render_template("tarefas.html")
     
-app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
